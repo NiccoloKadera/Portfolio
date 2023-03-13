0db9799679359e713b05fb4da33cb2b0300f5ff4
NiccoloKadera <106066913+NiccoloKadera@users.noreply.github.com>
2023-03-13 19:18:37 +0100

    Add files via upload

--- BlurrEditor.js
+++ BlurrEditor.js
@@ -9,12 +9,11 @@ window.addEventListener('scroll', () => {
         console.log("Sotto")
         document.getElementById("BgBlurr").classList.remove('OpZero');
         document.getElementById("BgBlurr").classList.add('OpOne');
-        document.getElementById("BackToTop").classList.remove('BackToTopHidden');
+        
     }
     if (Math.ceil(scrolled) <= 300) {
         document.getElementById("BgBlurr").classList.remove('OpOne');
         document.getElementById("BgBlurr").classList.add('OpZero');
-        document.getElementById("BackToTop").classList.add('BackToTopHidden');
     }
     
 
