46c1a826a2a03484f845bad3a0a9bcc01894ce1f
NiccoloKadera <106066913+NiccoloKadera@users.noreply.github.com>
2023-03-14 23:25:36 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,27 @@ workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
 );
+
+// Change this to your repository name
+var GHPATH = '/Portfolio';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'nB';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/main.css`,
+  `${GHPATH}/indexEng.html`,
+  `${GHPATH}/manifest.json`,
+  `${GHPATH}/BlurrEditorENG.js`,
+  `${GHPATH}/BlurrEditorITA.js`,
+]
