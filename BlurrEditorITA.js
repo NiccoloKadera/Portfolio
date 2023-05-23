8e8e234a3cc04940bb5e3878532f8d7b76f4b6ed
NiccoloKadera <106066913+NiccoloKadera@users.noreply.github.com>
2023-05-23 23:25:34 +0200

    Update BlurrEditorITA.js

--- BlurrEditorITA.js
+++ BlurrEditorITA.js
@@ -2,6 +2,7 @@
 var Backgoud = true
 var InputBG = document.getElementById("InputMode")
 var InputITA = document.getElementById("InputLangITA")
+const backToTop = document.getElementById("BackToTop")
 
 var blurr = document.getElementById("BgBlurr")
 
@@ -13,12 +14,15 @@ window.addEventListener('scroll', () => {
         document.getElementById("BgBlurr").classList.remove('OpZero');
         document.getElementById("BgBlurr").classList.add('OpOne');
         document.getElementById("InputMode").style.display = "none"
-        
+        backToTop.classList.remove('BackToTopHidden')
+        backToTop.classList.add('BackToTopVisible')
     }
     if (Math.ceil(scrolled) <= 300) {
         document.getElementById("BgBlurr").classList.remove('OpOne');
         document.getElementById("BgBlurr").classList.add('OpZero');
         document.getElementById("InputMode").style.display = "block"
+        backToTop.classList.remove('BackToTopVisible')
+        backToTop.classList.add('BackToTopHidden')
     }
 
     
